c7b0f1436a571d4011aedd51eecd0715a570e2c6
tai.letan <tlt84@open.ac.uk>
2025-07-09 17:41:20 +0700

    IR4130723 - No 'markdown only (aka not using HTML formatting) mode for non forum VLE tools #896099

--- backup/moodle2/backup_oublog_stepslib.php
+++ backup/moodle2/backup_oublog_stepslib.php
@@ -57,7 +57,7 @@ class backup_oublog_activity_structure_step extends backup_activity_structure_st
         $post  = new backup_nested_element('post', array('id'), array('groupid', 'title',
                                                                              'message', 'timeposted', 'allowcomments',
                                                                              'timeupdated', 'lasteditedby', 'deletedby',
-                                                                             'timedeleted', 'visibility'));
+                                                                             'timedeleted', 'visibility', 'messageformat'));
 
         $ratings = new backup_nested_element('ratings');
 
@@ -67,7 +67,8 @@ class backup_oublog_activity_structure_step extends backup_activity_structure_st
         $comments = new backup_nested_element('comments');
         $comment  = new backup_nested_element('comment', array('id'), array('userid', 'title', 'message',
                                                                                    'timeposted', 'deletedby', 'timedeleted',
-                                                                                    'authorname', 'authorip', 'timeapproved'));
+                                                                                    'authorname', 'authorip', 'timeapproved',
+                                                                                    'messageformat'));
 
         $edits = new backup_nested_element('edits');
         $edit  = new backup_nested_element('edit', array('id'), array('userid', 'oldtitle',
